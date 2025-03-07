2f31479e13998c11600c70709724a5fb6f9ed01a
hieule <hieult57@fpt.com>
2025-03-07 08:51:49 +0700

    fix : update Time Expiry

--- Services/ISeatLockService.cs
+++ Services/ISeatLockService.cs
@@ -4,4 +4,7 @@ using G5_MovieTicketBookingSystem.Models;
 public interface ISeatLockService
 {
     Task<SeatLock?> GetLatestSeatLockByUserIdAsync(int? userId);
+
+    Task UpdateExpirytimeByUserIdAsync(int? userId, DateTime expiryTime);
+    Task UpdateStarttimeByUserIdAsync(int? userId, DateTime expiryTime);
 }
