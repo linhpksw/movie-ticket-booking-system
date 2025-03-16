3d5b7aaa9203a346cded5c02ab93d8a6a1363a87
Trọng Linh <linhpksw@gmail.com>
2025-03-16 16:20:28 +0700

    feat: implement seat lock

--- Repositories/Impl/SeatLockRepository.cs
+++ Repositories/Impl/SeatLockRepository.cs
@@ -1,5 +1,5 @@
 using G5_MovieTicketBookingSystem.Models;
-﻿using G5_MovieTicketBookingSystem.Commons;
+using G5_MovieTicketBookingSystem.Commons;
 using G5_MovieTicketBookingSystem.Data;
 using Microsoft.EntityFrameworkCore;
 
